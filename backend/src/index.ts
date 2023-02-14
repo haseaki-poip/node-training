f6135911559a69c49aff5aa1e5b762abd16ef793
Hasegawa-Akito <h.akito1999@gmail.com>
2023-02-14 13:58:48 +0900

    webSocket確認

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,16 @@
 import express from "express";
+import WebSocket from "ws";
+
 const app: express.Express = express();
+const wss = new WebSocket.Server({ port: 8001 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+    ws.send("send message");
+  });
+  ws.send("connecting");
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
