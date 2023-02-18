4b18e1602c59cb1cdddb22b4345ee57fd7159989
Hasegawa-Akito <h.akito1999@gmail.com>
2023-02-18 23:34:03 +0900

    apiのルーティング

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import WebSocket from "ws";
+import roomRouter from "./routes/api/room";
 
 const app: express.Express = express();
+app.use(express.json());
 const wss = new WebSocket.Server({ port: 8001 });
 
 wss.on("connection", function connection(ws) {
@@ -10,11 +12,17 @@ wss.on("connection", function connection(ws) {
     ws.send("send message");
   });
   ws.send("connecting");
+
+  ws.on("close", function () {
+    console.log("ブラウザを閉じました");
+  });
 });
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
 });
+
+app.use("/api/room", roomRouter);
 
 app.listen(8000, () => {
   console.log("ポート8000番で起動しました。");
